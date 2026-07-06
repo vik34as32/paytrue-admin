2487e46fddac3de3fdc6078e923153f708012cb7
vikas kumar <vik23641@gmail.com>
2026-07-06 15:03:40 +0530

    new change uplaod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   webpack: (config, { dev }) => {
     if (dev) {
       // Prevent broken pages when .next cache is cleared while dev server is running
